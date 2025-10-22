36ec88e34547eacfbd498b75b7d6e14b0be2cfe7
lahirudilhara <galahirudilhara@gmail.com>
2025-10-22 18:08:59 +0530

    remove the sqlite configuration and added to use postgresql configurations

--- app/config.py
+++ app/config.py
@@ -1,9 +1,30 @@
 from pathlib import Path
+from dotenv import load_dotenv
+from app.log import logger
+import os
 
-# IAMLite SQLite Database Configuration
-SQLITE_DB_PATH = Path(f"{__file__}/../db/iam_admins.db").resolve()
-SQLITE_CONFIG_TABLE_NAME = "iam_config"
-SQLITE_ADMINS_TABLE_NAME = "iam_admins"
+load_dotenv()
 
 # Application Configuration
 APP_TITLE = "IAMLite"
+
+# Postgresql Database Configuration
+POSTGRES_DB_HOST = None
+POSTGRES_DB_PORT = None
+POSTGRES_DB_NAME = None
+POSTGRES_DB_USER = None
+POSTGRES_DB_PASSWORD = None
+
+def init_config()->None:
+    global POSTGRES_DB_HOST, POSTGRES_DB_PORT, POSTGRES_DB_NAME, POSTGRES_DB_USER, POSTGRES_DB_PASSWORD, APP_TITLE
+    POSTGRES_DB_HOST = os.getenv("POSTGRES_DB_HOST")
+    POSTGRES_DB_PORT = os.getenv("POSTGRES_DB_PORT")
+    POSTGRES_DB_NAME = os.getenv("POSTGRES_DB_NAME")
+    POSTGRES_DB_USER = os.getenv("POSTGRES_DB_USER")
+    POSTGRES_DB_PASSWORD = os.getenv("POSTGRES_DB_PASSWORD")
+    APP_TITLE = os.getenv("APP_TITLE", APP_TITLE)
+
+    if not all([POSTGRES_DB_HOST, POSTGRES_DB_PORT, POSTGRES_DB_NAME, POSTGRES_DB_USER, POSTGRES_DB_PASSWORD]):
+        logger.error("Database configuration is incomplete. Please set all required environment variables.")
+        logger.error(f"POSTGRES_DB_HOST: {POSTGRES_DB_HOST}. POSTGRES_DB_PORT: {POSTGRES_DB_PORT}. POSTGRES_DB_NAME: {POSTGRES_DB_NAME}. POSTGRES_DB_USER: {POSTGRES_DB_USER}. POSTGRES_DB_PASSWORD: {'SET' if POSTGRES_DB_PASSWORD else 'NOT SET'}.")
+        raise EnvironmentError("Database configuration is incomplete. Please set all required environment variables.")
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from app.db.db_initializer import initialize_database
 from app.api.admin import router as admin_router
+from app.config import init_config
+
+# Initialize configurations
+init_config()
 
 # Initialize the database
-initialize_database()
+# initialize_database()
 
 
 app = FastAPI(title="IAMLite")
